d6733705d4411c30966243be93666cce7b8a5fd7
uc13me-debug <uc13me@gmail.com>
2026-06-28 14:15:11 +0000

    Ignore ESLint and linting during build

--- apps/client/next.config.ts
+++ apps/client/next.config.ts
@@ -1,7 +1,12 @@
 const nextConfig = {
-  output: 'export', // Forces creation of the /out folder
+  output: 'export',
   images: {
-    unoptimized: true, // Required for static export
+    unoptimized: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 };
 export default nextConfig;
